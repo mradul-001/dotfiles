c2d536e4367319c3f19aa432d295a85b60e9af9a
mradul-001 <sonkarmradul@gmail.com>
2024-12-21 16:31:03 +0530

    Changed foot's transparency, swaylock background, nvim transparency

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,7 +1,34 @@
 return {
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				transparent_background = true, -- Enable transparency
+			})
+		end,
+	},
 	{ "dracula/vim", name = "dracula", priority = 1000 },
-	{ "ellisonleao/gruvbox.nvim", name = "gruvbox", priority = 1000 },
+	{
+		"ellisonleao/gruvbox.nvim",
+		name = "gruvbox",
+		priority = 1000,
+		config = function()
+			require("gruvbox").setup({
+				transparent_mode = true, -- Enable transparency
+			})
+		end,
+	},
 	{ "nyoom-engineering/oxocarbon.nvim", name = "oxocarbon", priority = 1000 },
-	{ "rose-pine/neovim", name = "rose-pine", priority = 1000 },
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		priority = 1000,
+		config = function()
+			require("rose-pine").setup({
+				disable_background = true, -- Enable transparency
+			})
+		end,
+	},
 }
